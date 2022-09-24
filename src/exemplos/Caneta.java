5c2ca9e36f0b67f2b2f7ee53de062bdc90a2258e
unknown <renatosaragov@htmail.com>
2022-09-24 11:43:47 -0300

    Exercicio Caneta

--- src/exemplos/Caneta.java
+++ src/exemplos/Caneta.java
@@ -1,21 +1,34 @@
 package exemplos;
+
 public class Caneta {
-    String cor = "Sem Cor";
-    String pontaCaneta = "Ponta fina";
+    String cor = "SemCor";
+    String ponta = "fina";
+    String marca;
 
-    public void trocarCor(){
-
+    public void trocaCor(String corCliente) throws Exception {
+        if(corCliente.equals("AZUL")|| corCliente.equals("VERMELHO") ||corCliente.equals("PRETO")){
+            cor =  corCliente;
+        }else{
+            throw new Exception("Cor invalida");
+        }
     }
 
     public void rabiscar(){
-
+        System.out.println("Rabiscando com a cor " + cor);
     }
-
     public void pintar(){
-
+        System.out.println("Pintando com a cor " + cor);
     }
 
-    public void escrever(){
+    public void escrever(String texto){
+        System.out.println("Escrevendo com a cor " + cor + " "+ texto);
+    }
 
+    public String mostrarCorAtual(){
+          return cor;
+    }
+
+    public void inserirMarca(String marcaCliente){
+        marca  = marcaCliente;
     }
 }
--- src/main/Execucao.java
+++ src/main/Execucao.java
@@ -1,9 +1,17 @@
 package main;
 
+import exemplos.Caneta;
 import exemplos.MetodosSemMain;
 
 public class Execucao {
-    public static void main(String[] args) {
-        MetodosSemMain metodo= new MetodosSemMain();
+    public static void main(String[] args) throws Exception {
+        Caneta caneta = new Caneta();
+        caneta.inserirMarca("Palmeirex");
+        caneta.trocaCor("ROSA");
+        System.out.println("Cor Atual " + caneta.mostrarCorAtual());
+        caneta.rabiscar();
+        caneta.pintar();
+        caneta.escrever("Aula de Automação");
+
     }
 }
