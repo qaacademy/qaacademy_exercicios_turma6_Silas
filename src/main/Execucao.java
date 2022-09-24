61f57f67c0ab35d30b9fcfcfa256614f8324d87b
unknown <renatosaragov@htmail.com>
2022-09-24 17:13:38 -0300

    Exec medium exemplo

--- src/main/Execucao.java
+++ src/main/Execucao.java
@@ -10,12 +10,15 @@ import exercicios_medium.Exec1;
 
 public class Execucao {
     public static void main(String[] args) {
-        Exec5 exec5 = new Exec5();
-        int num1 = 3, num2 =3, num3 = 3;
-        System.out.println("A soma é " +exec5.somar(num1,num2,num3));
-        System.out.println("A sub é " +exec5.sub(num1,num2,num3));
-        System.out.println("A mult é " +exec5.mult(num1,num2,num3));
-        System.out.println("A media é " +exec5.media(num1,num2,num3));
+
+        Exec1 exec1 = new Exec1();
+        exec1.exibirParImpar();
+//        Exec5 exec5 = new Exec5();
+//        int num1 = 3, num2 =3, num3 = 3;
+//        System.out.println("A soma é " +exec5.somar(num1,num2,num3));
+//        System.out.println("A sub é " +exec5.sub(num1,num2,num3));
+//        System.out.println("A mult é " +exec5.mult(num1,num2,num3));
+//        System.out.println("A media é " +exec5.media(num1,num2,num3));
 //        Exec4 exec4 = new Exec4();
 //        System.out.println("O dobro é: " + exec4.calcularDobro(10));
 //        Exec1 exec1 = new Exec1();
